78b048f4106cd12b9acd2c55fbb28f74ca4250d1
EduardoTartas <eduardos.tartas@gmail.com>
2025-10-23 22:32:05 -0400

    ajustando envio de emails

---
+++ src/utils/templates/emailTemplates.js
@@ -0,0 +1,69 @@
+import "dotenv/config";
+
+export const emailDeBoasVindas = (data) => ({
+  to: data.email,
+  subject: "Bem-vindo ao IFRO Events!",
+  template: "bemvindo",
+  data: {
+    nome: data.nome,
+    logoUrl: "https://i.imgur.com/pHjP2qy.png",
+    mensagem: "Bem-vindo ao IFRO Events! Estamos felizes em tê-lo conosco.",
+    mostrarBotao: true,
+    textoBotao: "Começar",
+    urlBotao: `${process.env.FRONTEND_URL}/login`
+  }
+});
+
+// -----------
+
+export const emailRecover = (data) => ({
+  to: data.email,
+  subject: "Recuperação de Senha - IFRO Events",
+  template: "generico",
+  data: {
+    logoUrl: "https://i.imgur.com/pHjP2qy.png",
+    mostrarHeader: true,
+    corPrimaria: "#4338CA",
+    nome: data.nome,
+    titulo: "Recuperação de Senha",
+    mensagem:
+      "Recebemos uma solicitação para redefinir sua senha. Clique no botão abaixo para prosseguir com a recuperação de sua conta.",
+    textoDestaque: "Este link expira em 1 hora.",
+    mostrarBotao: true,
+    textoBotao: "Alterar Senha",
+    urlBotao: `${process.env.FRONTEND_URL}/recuperar_senha/${data.token}`,
+    corBotao: "#4338CA",
+  },
+});
+
+// -----------
+
+export const emailCompartilhamentoDono = (data) => ({
+  to: data.email,
+  subject: "Compartilhamento de permissão - IFRO Events",
+  template: "generico",
+  data: {
+    logoUrl: "https://i.imgur.com/pHjP2qy.png",
+    mostrarHeader: true,
+    corPrimaria: "#4338CA",
+    nome: data.nomeDono,
+    titulo: "Compartilhamento de permissão",
+    mensagem:
+      `Você concedeu permissões de acesso ao evento "${data.evento}" para ${data.nome}. Caso não tenha realizado essa ação, entre em contato com o suporte.`,
+  },
+});
+
+export const emailCompartilhamento = (data) => ({
+  to: data.email,
+  subject: "Compartilhamento de permissão - IFRO Events",
+  template: "generico",
+  data: {
+    logoUrl: "https://i.imgur.com/pHjP2qy.png",
+    mostrarHeader: true,
+    corPrimaria: "#4338CA",
+    nome: data.nome,
+    titulo: "Compartilhamento de permissão",
+    mensagem:
+      `Você recebeu permissões de acesso ao evento "${data.evento}" concedidas por ${data.nomeDono}. Caso não reconheça essa ação, entre em contato com o suporte.`,
+  },
+});
